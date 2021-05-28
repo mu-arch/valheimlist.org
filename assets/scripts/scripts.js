412ac2731f2423ec8435c00bdc6e4b8463d9105d
cybermancy <saurek115@gmail.com>
2021-05-28 16:59:49 -0500

    near complete listing page

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -3,18 +3,6 @@ function e(e) {
     return document.getElementById(e);
 }
 
-/*
-e("search-id").addEventListener('input', showToolTipSearch);
-function showToolTipSearch() {
-    console.log(e("search-id").value.length)
-    if (e("search-id").value.length > 0) {
-        e("ttsearch").style.opacity = "1";
-    } else {
-        e("ttsearch").style.opacity = "0";
-    }
-}
- */
-
 window.addEventListener('scroll', function() {
     if (window.pageYOffset > 300) {
         e("sidebar-content").style.position = "fixed";
